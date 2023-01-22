095a574a98639c74be14efc34ee2343362b21bdb
kovacsmate88 <kovacsmate321@gmail.com>
2023-01-23 00:37:12 +0100

    started create test with Mockito

--- src/test/java/com/codecool/stockTrader/StockAPIServiceTest.java
+++ src/test/java/com/codecool/stockTrader/StockAPIServiceTest.java
@@ -1,6 +1,12 @@
 package com.codecool.stockTrader;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class StockAPIServiceTest {
 
@@ -20,8 +26,12 @@ class StockAPIServiceTest {
     }
 
     @Test
-    void getPrice_malformedResponseFromAPI_throwsJSONException() {
-
+    void getPrice_malformedResponseFromAPI_throwsJSONException() throws IOException {
+        RemoteURLReader reader = mock(RemoteURLReader.class);
+        StockAPIService service = new StockAPIService();
+        service.setReader(reader);
+        when(reader.readFromUrl("https://run.mocky.io/v3/9e14e086-84c2-4f98-9e36-54928830c980?stock=abc")).thenReturn("");
+        assertThrows(JSONException.class, () -> service.getPrice("abc"));
     }
 
 }
