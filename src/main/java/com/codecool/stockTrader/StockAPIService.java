7f8cead9af681f83412ff99b62691d6024f3ad04
kovacsmate88 <kovacsmate321@gmail.com>
2023-01-23 00:36:39 +0100

    create setreader method

--- src/main/java/com/codecool/stockTrader/StockAPIService.java
+++ src/main/java/com/codecool/stockTrader/StockAPIService.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class StockAPIService {
 
 	private static final String apiPath = "https://run.mocky.io/v3/9e14e086-84c2-4f98-9e36-54928830c980?stock=%s";
+	private RemoteURLReader reader = new RemoteURLReader();
 	
 	/** Get stock price from iex and return as a double
-     *  @param symbol Stock symbol, for example "aapl"
+     *  @param symbol Stock symbol, for example "apl"
      **/
 	public double getPrice(String symbol) throws IOException {
         String url = String.format(apiPath, symbol);
@@ -26,5 +27,9 @@ public class StockAPIService {
 	public boolean buy(String symbol) {
 		// Stub. No need to implement this.
 		return true;
+	}
+
+	public void setReader(RemoteURLReader reader) {
+		this.reader = reader;
 	}
 }
